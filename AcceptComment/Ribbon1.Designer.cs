1e26179d722c00f3dccf2bf746ea4355f956ccf4
Pechy <pechmanpavel@email.cz>
2022-05-11 18:08:08 +0200

    Add project files.

---
+++ AcceptComment/Ribbon1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace AcceptComment
+{
+    partial class rbnAcceptComment : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public rbnAcceptComment()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.grpAcceptComment = this.Factory.CreateRibbonGroup();
+            this.btnAccept = this.Factory.CreateRibbonButton();
+            this.tBoxStart = this.Factory.CreateRibbonEditBox();
+            this.tBoxEnd = this.Factory.CreateRibbonEditBox();
+            this.chBoxDelete = this.Factory.CreateRibbonCheckBox();
+            this.tab1.SuspendLayout();
+            this.grpAcceptComment.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.grpAcceptComment);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // grpAcceptComment
+            // 
+            this.grpAcceptComment.Items.Add(this.btnAccept);
+            this.grpAcceptComment.Items.Add(this.tBoxStart);
+            this.grpAcceptComment.Items.Add(this.tBoxEnd);
+            this.grpAcceptComment.Items.Add(this.chBoxDelete);
+            this.grpAcceptComment.Label = "Accept Comment";
+            this.grpAcceptComment.Name = "grpAcceptComment";
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnAccept.Label = "Přijmout komentář";
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.ShowImage = true;
+            this.btnAccept.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnAccept_Click);
+            // 
+            // tBoxStart
+            // 
+            this.tBoxStart.Label = "Počáteční řetězec";
+            this.tBoxStart.MaxLength = 20;
+            this.tBoxStart.Name = "tBoxStart";
+            this.tBoxStart.SizeString = "Maxmilián Skočdopole: ";
+            this.tBoxStart.Text = "(rev.0: ";
+            // 
+            // tBoxEnd
+            // 
+            this.tBoxEnd.Label = "Koncový řetězec";
+            this.tBoxEnd.MaxLength = 20;
+            this.tBoxEnd.Name = "tBoxEnd";
+            this.tBoxEnd.SizeString = "Maxmilián Skočdopole: ";
+            this.tBoxEnd.Text = ")";
+            // 
+            // chBoxDelete
+            // 
+            this.chBoxDelete.Label = "Po přijetí smazat";
+            this.chBoxDelete.Name = "chBoxDelete";
+            this.chBoxDelete.ScreenTip = "Po přijetí komentáře ho z buňky smaže";
+            // 
+            // rbnAcceptComment
+            // 
+            this.Name = "rbnAcceptComment";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.grpAcceptComment.ResumeLayout(false);
+            this.grpAcceptComment.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpAcceptComment;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnAccept;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox tBoxStart;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox tBoxEnd;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox chBoxDelete;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal rbnAcceptComment Ribbon1
+        {
+            get { return this.GetRibbon<rbnAcceptComment>(); }
+        }
+    }
+}
---
+++ AcceptComment/Ribbon1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace AcceptComment
+{
+    public partial class rbnAcceptComment
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnAccept_Click(object sender, RibbonControlEventArgs e)
+        {
+            AcceptComment();
+        }
+
+        public void AcceptComment()
+        {
+            Excel.Range selection = Globals.ThisAddIn.Application.Selection;
+            String comment;
+
+            foreach (Excel.Range cell in selection)
+            {
+                if (cell.Comment != null)
+                {
+                    comment = cell.Comment.Text();
+                    cell.Value = comment.Substring(tBoxStart.Text.Length, comment.Length - tBoxStart.Text.Length - tBoxEnd.Text.Length);
+                    if (chBoxDelete.Checked)
+                    {
+                        cell.Comment.Delete();
+                    }
+                }
+            }
+        }
+    }
+}
